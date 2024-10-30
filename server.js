43e76ee9e89652e59ffba9b8e4d389aabda3a295
piro-piyush <piyush72717272@gmail.com>
2024-10-30 22:39:33 +0530

    update server.js & made connection with mongoose

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-
 // Initialization
-const express = require('express');
+const express = require("express");
 const app = express();
 
-// App routes
-app.get("/", function(req, res){
-    res.send("This is home page");
-});
+const mongoose = require("mongoose");
 
-app.get("/", function(req, res){
-    res.send("This is home page");
-});
+mongoose
+  .connect(
+    "mongodb+srv://piyush:72717272@cluster0.ju8v3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(function () {
+    // App routes
+    app.get("/", function (req, res) {
+      res.send("This is home page");
+    });
+
+    app.get("/", function (req, res) {
+      res.send("This is home page");
+    });
+  });
 
 // Starting the server on a PORT
-app.listen(5000, function(){
-    console.log("Server is running at PORT : 5000");
+app.listen(5000, function () {
+  console.log("Server is running at PORT : 5000");
 });
