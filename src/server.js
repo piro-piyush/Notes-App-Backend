efffd13e10ba627b7b4c37fc54a4af0b86d28ac6
piro-piyush <piyush72717272@gmail.com>
2024-10-31 15:53:40 +0530

    uploading on cloud

--- src/server.js
+++ src/server.js
@@ -8,11 +8,9 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
+const mongoDbPath = "mongodb+srv://piyush:72717272@cluster0.ju8v3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongoose
-  .connect(
-    "mongodb+srv://piyush:72717272@cluster0.ju8v3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
+  .connect(mongoDbPath)
   .then(function () {
 
     // App routes
@@ -26,7 +24,8 @@ mongoose
   });
 
 // Starting the server on a PORT
-app.listen(5000, function () {
-  console.log("Server is running at PORT : 5000");
-  console.log("\nClick here to view : http://localhost:5000");
+const PORT = process.env.PORT || 5000 ;
+app.listen(PORT, function () {
+  console.log("Server is running at PORT : PORT");
+  console.log("\nClick here to view : http://localhost:PORT");
 });
